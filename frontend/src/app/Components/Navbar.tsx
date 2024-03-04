948569ba5fcfb78446757cfc2a8187405408445b
Munkherdene <b.munkherdene28@gmail.com>
2024-03-04 10:17:40 +0800

    add navbar

--- frontend/src/app/Components/Navbar.tsx
+++ frontend/src/app/Components/Navbar.tsx
@@ -1,7 +1,15 @@
-export default function Navbar(){
-    return (
-        <div>
-            
-        </div>
-    )
+import Logo from "../Logo/Logo";
+import Bell from "../Logo/Bell";
+import User from "../Logo/User";
+export default function Navbar() {
+  return (
+    <div className="bg-black w-full h-[48px] flex justify-between items-center p-4">
+      <Logo />
+      <div className="flex items-center">
+        <Bell />
+        <User />
+        <p>Username</p>
+      </div>
+    </div>
+  );
 }
