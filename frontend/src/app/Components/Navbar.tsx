830d4afcc58b22dc28bfa45d72fffcc9e6281cf2
Munkh-Erdene Beleg-Urnukh <23LP1379@G303-CC0140.local>
2024-03-05 10:47:07 +0800

    edited navbar router

--- frontend/src/app/Components/Navbar.tsx
+++ frontend/src/app/Components/Navbar.tsx
@@ -1,14 +1,17 @@
+'use client'
 import Logo from "../Logo/Logo";
 import Bell from "../Logo/Bell";
 import User from "../Logo/User";
+import { useRouter } from "next/navigation";
 export default function Navbar() {
+    const router = useRouter();
     return (
         <div className="bg-black h-[48px] flex justify-between items-center p-4">
             <Logo />
             <div className="flex items-center">
                 <Bell />
                 <User />
-                <p className="text-white">Username</p>
+                <p className="text-white cursor-pointer" onClick={() => { router.push("/user") }}>Username</p>
             </div>
         </div>
     );
