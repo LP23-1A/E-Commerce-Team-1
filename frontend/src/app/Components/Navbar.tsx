da2efa6eb97729f783a61ee8a913fbc2898302ea
Munkh-Erdene Beleg-Urnukh <23LP1379@G303-CC0140.local>
2024-03-05 09:31:53 +0800

    make Navbar

--- frontend/src/app/Components/Navbar.tsx
+++ frontend/src/app/Components/Navbar.tsx
@@ -2,14 +2,14 @@ import Logo from "../Logo/Logo";
 import Bell from "../Logo/Bell";
 import User from "../Logo/User";
 export default function Navbar() {
-  return (
-    <div className="bg-black w-full h-[48px] flex justify-between items-center p-4">
-      <Logo />
-      <div className="flex items-center">
-        <Bell />
-        <User />
-        <p>Username</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="bg-black h-[48px] flex justify-between items-center p-4">
+            <Logo />
+            <div className="flex items-center">
+                <Bell />
+                <User />
+                <p className="text-white">Username</p>
+            </div>
+        </div>
+    );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 import Navbar from "./Components/Navbar";
 export default function Home() {
   return (
-    <div className="bg-white w-full h-full">
+    <div>
       <Navbar />
     </div>
   );
