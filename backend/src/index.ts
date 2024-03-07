862f3a5317131fd7d31f04e1004fb42918a22ab2
Ангараг Батдэлгэр <23LP7433@G303-CC0139.local>
2024-03-07 11:47:33 +0800

    income backend done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import { connectDataBase } from "./utils/database";
+import { connectDataBase } from "./utils/Database";
 import { order } from "./router/order";
 import { product } from "./router/Product";
 import { user } from "../src/router/user";
@@ -6,15 +6,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import bp from "body-parser";
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-import { user } from "../src/router/user";
-import { product } from "./router/Product";
-=======
 import { Income } from "./router/income";
->>>>>>> 49815fb (income backend done)
->>>>>>> 1d9a58c (income backend done)
 
 dotenv.config();
 
@@ -28,13 +20,10 @@ const PORT = process.env.PORT || 8000;
 
 app.use(bp.json());
 app.use(cors());
-<<<<<<< HEAD
 app.use('/user', user)
 app.use("/product", product)
 app.use('/order',order);
-=======
 app.use("/Income", Income);
->>>>>>> 49815fb (income backend done)
 
 app.get("/", (_, res) => {
   res.status(200).send({
