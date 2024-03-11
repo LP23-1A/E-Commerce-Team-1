c56156f37a2aefea800819ec729ff265dc4968dd
Munkh-Od Batsukh <23LP5760@G303-CC0157.local>
2024-03-11 11:49:27 +0800

    filterByDate

---
+++ frontend/src/Components/FilterByDate.tsx
@@ -0,0 +1,10 @@
+import { useRouter } from "next/navigation"
+
+export default function FilterByDate() {
+    const router = useRouter();
+    return (
+        <div>
+         
+        </div>
+    );
+}
