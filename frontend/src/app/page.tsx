91dd4ed8215080888cb4f05a8b7ba177d45ae393
Ангараг Батдэлгэр <23LP7433@G303-CC0139.local>
2024-03-11 12:11:39 +0800

    dashboard add

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 import SignUp from "./SignUp/page";
-import Navbar from "@/Components/Navbar";
-import Sidebar from "../Components/Sidebar";
-// import AddProduct from "@/Components/AddProduct";
 import Product from "@/app/Product/page";
 import OrderDetails from "./AdminDashboard/Order/orderDetails/page";
 import Order from "./AdminDashboard/Order/page";
+import Navbar from "@/Components/Navbar";
+import Sidebar from "../Components/Sidebar";
+import Dashbo from "@/Components/Dashboard";
 
 export default function Home() {
   return (
