ab9def988fae0cec6503f1d9f557537ad4e05579
angarag6 <anguu0622lq7@gmail.com>
2024-03-14 02:13:54 +0800

    asdf

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,30 @@
+<<<<<<< HEAD
 "use client"
+=======
+"use client";
+>>>>>>> 9917b5f (asdf)
 import SignUp from "./SignUp/page";
 import Navbar from "@/Components/Navbar";
 import Sidebar from "../Components/Sidebar";
 import Product from "@/app/Product/page";
+<<<<<<< HEAD
 import OrderDetails from "./OrderDetails/page";
 import Order from "./Order/page";
+=======
+import OrderDetails from "./AdminDashboard/Order/orderDetails/page";
+import Order from "./AdminDashboard/Order/page";
+import Income from "../Components/Income";
+>>>>>>> 9917b5f (asdf)
 
 export default function Home() {
   return (
     <div>
+<<<<<<< HEAD
       <Order />
+=======
+      <Navbar />
+      <Sidebar />
+>>>>>>> 9917b5f (asdf)
     </div>
   );
 }
