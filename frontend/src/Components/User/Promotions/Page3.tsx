749107ae8c7eba3857f36eff71dfe35479826b94
Bayra1 <145735796+Bayra1@users.noreply.github.com>
2024-03-26 09:36:08 +0800

    55 work on client side  carousel (#56)
    * working on carousel section and lacking of detail about the selected section on that
    
    * carousel is done and more detail about that is getting confused
    
    * carousel is ready to go
    
    ---------
    
    Co-authored-by: Ikhbayar Sukhbaatar <23LP4139@G303-CC0130.local>

---
+++ frontend/src/Components/User/Promotions/Page3.tsx
@@ -0,0 +1,69 @@
+import { FetchAllProducts } from "../Api/FetchAllProducts";
+import { useRouter } from "next/navigation";
+import useSWR from "swr";
+import IsLoading from "../Isloading";
+import toast from "react-hot-toast";
+import ControlButton3 from "./ControlButton3";
+
+export default function Page3({ nextPage, previousPage }: any) {
+  const { data, isLoading, error } = useSWR("/product/get", FetchAllProducts);
+  const router = useRouter();
+
+  if (isLoading) return <IsLoading />;
+
+  if (error) {
+    toast.error("Failed to fetch All Products from backend");
+    console.error(error);
+    return null;
+  }
+
+  return (
+    <>
+      {data &&
+        data.slice(3, 4).map((element: any, index: number) => (
+          <div key={index} className="flex justify-center items-center w-[80%]">
+            <div className="flex w-full justify-between">
+              <div className="w-5/12 flex flex-col gap-[10px]">
+                <div
+                  style={{ color: "#FB2E86" }}
+                  className="font-bold text-base mb-[5px]"
+                >
+                  A comfortable environment in your life
+                </div>
+                <p
+                  style={{ fontSize: "53px" }}
+                  className="font-extrabold text-black w-[480px] h-[128px] flex justify-center items-center"
+                >
+                  {element.productName}
+                </p>
+                <h6
+                  style={{ color: "#8A8FB9" }}
+                  className="font-bold text-base w-[385px] h-[56px]"
+                >
+                  {element.description}
+                </h6>
+                <button
+                  onClick={() =>
+                    router.push(`/User/Details/=specificInfo=?${element._id}`)
+                  }
+                  style={{ backgroundColor: "#FB2E86", borderRadius: "5px" }}
+                  className="w-[163px] h-[50px] text-white"
+                >
+                  More Detail about
+                </button>
+              </div>
+              <div className="w-4/12">
+                <img
+                  style={{ borderRadius: "10px" }}
+                  className="w-4/12 bg-green-400 w-[350px] h-[350px]"
+                  src={element.images}
+                  alt=""
+                />
+              </div>
+            </div>
+            <ControlButton3 nextPage={nextPage} previousPage={previousPage} />
+          </div>
+        ))}
+    </>
+  );
+}
