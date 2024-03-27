7048084e5ce88e8caab5dda08e4400b95cbe688a
Bayra1 <isvhbaatar5@gmail.com>
2024-03-27 23:36:50 +0800

    some changes are made

--- frontend/src/Components/User/Comment.tsx
+++ frontend/src/Components/User/Comment.tsx
@@ -3,6 +3,7 @@ import { StarIcon } from "./Icon";
 
 export default function CommentSection({ afterComment }: any) {
   const data = afterComment.map(JSON.parse);
+  console.log(data);
 
   return (
     <div className="flex flex-col gap-[30px]">
@@ -20,26 +21,24 @@ export default function CommentSection({ afterComment }: any) {
         className="flex flex-col gap-[21px] rounded-lg bg-[#fff]"
         style={{ padding: "48px 24px" }}
       >
-        {data.map(
-          (element: { comment: string; rating: number }, index: number) => (
-            <div
-              key={index}
-              className="border-dashed border-b-2 pb-3 gap-[20px] flex flex-col"
-            >
-              <div className="flex flex-row gap-[10px]">
-                {[...Array(5)].map((_, index) => (
-                  <span key={index} className="star">
-                    {index < element.rating ? "★" : "☆"}
-                  </span>
-                ))}
-              </div>
-              <div className="font-extrabold text-lg non-italic text-[#1D3178]">
-                Some random names here
-              </div>
-              <div>{element.comment}</div>
+        {data.map((element: { comment: string; rating: number }, index: number) => (
+          <div
+            key={index}
+            className="border-dashed border-b-2 pb-3 gap-[20px] flex flex-col"
+          >
+            <div className="flex flex-row gap-[10px]">
+              {[...Array(5)].map((_, index) => (
+                <span key={index} className="star">
+                  {index < element.rating ? "★" : "☆"}
+                </span>
+              ))}
             </div>
-          )
-        )}
+            <div className="font-extrabold text-lg non-italic text-[#1D3178]">
+              Some random names here
+            </div>
+            <div>{element.comment}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
