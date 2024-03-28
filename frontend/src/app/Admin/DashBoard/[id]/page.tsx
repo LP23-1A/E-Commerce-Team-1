b633eba772a1adbde600974ad3c88d78426e694d
Ангараг Батдэлгэр <23LP7433@G303-CC0139.local>
2024-03-28 10:01:38 +0800

    dsf

--- frontend/src/app/Admin/DashBoard/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import Sidebar from "@/Components/Admin/Sidebar";
-
-export default function DashBoard() {
-    return (
-        <div>            
-            <Sidebar />
-        </div>
-    )
-}
