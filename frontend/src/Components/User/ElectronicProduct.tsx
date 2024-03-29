1e476e36c69628a6e3e4d65e478763a46616691e
Munkh-Erdene Beleg-Urnukh <23LP1379@G303-CC0140.local>
2024-03-29 11:29:11 +0800

    fixing details

--- frontend/src/Components/User/ElectronicProduct.tsx
+++ frontend/src/Components/User/ElectronicProduct.tsx
@@ -1,23 +1,14 @@
 "use client";
-import { useState } from "react";
-import MiniNavbar from "./UserMiniNavbar";
 import Products from "./Products";
 
 export default function ElectronicPro() {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const handleSearch = (term: any) => {
-    setSearchTerm(term);
-  };
-
   return (
     <div>
-      <MiniNavbar onSearch={handleSearch} />
       <div className="flex items-center h-[100px] bg-[#F6F5FF] px-[360px] gap-4">
         <p>Home</p>
         <p className="text-[#FB2E86]">Электрон бараа</p>
       </div>
-      <Products searchTerm={searchTerm} />
+      <Products />
     </div>
   );
 }
