1c8d0bd669a3618aef77d37b1485424ebfe72422
Bayra1 <isvhbaatar5@gmail.com>
2024-07-08 17:19:03 +0800

    test

--- frontend/src/Components/Admin/Alre-SignedUp.tsx
+++ frontend/src/Components/Admin/Alre-SignedUp.tsx
@@ -1,8 +1,10 @@
 export default function AlreSignedUp() {
-    return (
-        <div className="flex flex-row text-blacl justify-center gap-4 mt-7 w-[360px] items-center rounded-lg h-[56px] p-2">
-            Already Signed Up ?
-            <button className="hover:underline-offset-4 hover:underline hover:text-blue-500 underline underline-offset-4">Sign In</button>
-        </div>
-    )
+  return (
+    <div className="flex flex-row text-blacl justify-center gap-4 mt-7 w-[360px] items-center rounded-lg h-[56px] p-2">
+      Already Signed Up ?
+      <button className="hover:underline-offset-4 hover:underline hover:text-blue-500 underline underline-offset-4">
+        Sign In
+      </button>
+    </div>
+  );
 }
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,13 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { CartProvider } from "@/Components/User/CartContext";
 import AuthProvider from "@/Components/Admin/AuthProvider";
-import NavbarClient from "@/Components/User/NavbarClient";
 import React from "react";
 import "./globals.css";
-import Footer from "@/Components/User/Footer";
-import MiniNavbar from "@/Components/User/UserMiniNavbar";
-import { CartProvider } from "@/Components/User/CartContext";
-import Path from "@/Components/User/Path";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,9 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthProvider>
-          <CartProvider>
-            {children}
-          </CartProvider>
+          <CartProvider>{children}</CartProvider>
         </AuthProvider>
       </body>
     </html>
